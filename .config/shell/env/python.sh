3c3009c6ef146f8f5c4850165bedb985025d901e
gdario <git.gdario@gmail.com>
2019-12-28 18:34:43 +0100

    Update

--- .config/shell/env/python.sh
+++ .config/shell/env/python.sh
@@ -5,4 +5,10 @@ export WORKON_HOME="$HOME/code/.virtualenvs"
 export VIRTUALENVWRAPPER_PYTHON="/usr/bin/python3"
 export VIRTUALENVWRAPPER_VIRTUALENV="/usr/local/bin/virtualenv"
 
-source  /usr/local/bin/virtualenvwrapper_lazy.sh
+
+
+WRAPPER_LAZY_SCRIPT_LOCATION="/usr/local/bin/virtualenvwrapper_lazy.sh"
+
+if [ -e $WRAPPER_LAZY_SCRIPT_LOCATION ]; then
+  source $WRAPPER_LAZY_SCRIPT_LOCATION 
+fi
